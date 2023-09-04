0938ab7079fdccaf1579e8804927cb93782d0655
Traci Kamp <traci.kamp@gmail.com>
2023-09-03 17:03:14 -0700

    fix: uptake litter-api v2 (#27)
    uses the v2 release of litter-api
    
    BREAKING CHANGE: Removed several CLI flags that became irrelevant for litter-api v2
    
    Fix #26

--- go.mod
+++ go.mod
@@ -4,17 +4,16 @@ go 1.18
 
 require (
 	github.com/prometheus/client_golang v1.15.0
-	github.com/sirupsen/logrus v1.9.0
+	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.15.0
-	github.com/tlkamp/litter-api v1.0.4
+	github.com/tlkamp/litter-api/v2 v2.0.2
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/go-resty/resty/v2 v2.7.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -22,6 +21,7 @@ require (
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/procfs v0.9.0 // indirect
@@ -29,9 +29,8 @@ require (
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
-	golang.org/x/net v0.9.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
 	"strings"
@@ -13,22 +14,10 @@ import (
 	"github.com/spf13/viper"
 )
 
-// These values have been reverse-engineered from the mobile apps
-const (
-	defaultApiKey       = "p7ndMoj61npRZP5CVz9v4Uj0bG769xy6758QRBPb"
-	defaultClientId     = "IYXzWN908psOm7sNpe4G.ios.whisker.robots"
-	defaultClientSecret = "C63CLXOmwNaqLTB2xXo6QIWGwwBamcPuaul"
-)
-
 func init() {
 	flag.BoolP("help", "h", false, "print help and exit")
-	flag.String("api-key", defaultApiKey, "litter robot api key")
 	flag.String("email", "", "litter robot account email address")
 	flag.String("password", "", "litter robot account password")
-	flag.String("client-id", defaultClientId, "litter robot client id")
-	flag.String("client-secret", defaultClientSecret, "litter robot client secret")
-	flag.String("api-url", "", "litter robot API URL")
-	flag.String("auth-url", "", "litter robot auth URL")
 	flag.String("address", "0.0.0.0:9080", "the server address")
 	flag.String("log-level", "info", "the log level")
 
@@ -65,24 +54,24 @@ func parseLevelDefault(level string, def log.Level) log.Level {
 }
 
 func main() {
+
 	email := viper.GetString("email")
 	password := viper.GetString("password")
-	apiKey := viper.GetString("api-key")
-	clientSecret := viper.GetString("client-secret")
-	clientId := viper.GetString("client-id")
-	endpoint := viper.GetString("api-url")
-	authEndpoint := viper.GetString("auth-url")
 	address := viper.GetString("address")
 
-	lrc := NewCollector(email, password, apiKey, clientSecret, clientId, endpoint, authEndpoint)
-	ticker := time.NewTicker(lrc.lrClient.Expiry - (120 * time.Second))
+	ctx := context.Background()
+
+	lrc := NewCollector(ctx, email, password)
+	ticker := time.NewTicker((3600 * time.Second) - (120 * time.Second))
 	quit := make(chan struct{})
 
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				lrc.lrClient.RefreshToken()
+				if err := lrc.lrClient.RefreshToken(ctx); err != nil {
+					log.Error("error refreshing token", "error", err.Error())
+				}
 			case <-quit:
 				ticker.Stop()
 				return
